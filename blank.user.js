d141a65b58b32acc167d343e58d873a01a2d1057
moqiguzhu <moqiguzhu@163.com>
2017-01-10 15:05:02 +0800

    add blank script for test

---
+++ blank.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        SGAutoFarm
+// @namespace   com.sgamer.bbs.sgautofarm
+// @description SG自动伐木机
+// @include     http://bbs.sgamer.com/forum-*.html
+// @include     http://bbs.sgamer.com/thread-*.html
+// @include     http://bbs.sgamer.com/*mod=viewthread*
+	// @include     http://bbs.sgamer.com/*mod=forumdisplay*
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+
+(function() {
+
+})();
