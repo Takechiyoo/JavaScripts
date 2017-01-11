09e66bdda38ab6d0778eb44895583cb6eaba6e01
moqiguzhu <moqiguzhu@163.com>
2017-01-11 22:05:37 +0800

    test 36

--- SGAutoFarm.user.js
+++ SGAutoFarm.user.js
@@ -6,7 +6,7 @@
 // @include     http://bbs.sgamer.com/thread-*.html
 // @include     http://bbs.sgamer.com/*mod=viewthread*
 // @include     http://bbs.sgamer.com/*mod=forumdisplay*
-// @version     1.3.4
+// @version     1.3.5
 // @grant       none
 // ==/UserScript==
 
